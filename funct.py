1ce86544314c95d7011c8b8cb3a70d290dc72a7d
Amanchouhan192 <amanchouhan192@gmail.com>
2018-03-24 20:28:34 +0530

    function revsion

---
+++ funct.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+# Function definition is here
+def printme( str ):
+   "This prints a passed string into this function"
+   print str
+   return;
+
+def integer(int):
+    "this is the function only return the integer"
+    print int
+    return;
+
+# Now you can call printme function
+printme("I'm first call to user defined function!")
+printme("Again second call to the same function")
+integer(5)
